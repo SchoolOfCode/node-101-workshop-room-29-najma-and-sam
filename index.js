41f9967b6c91a9e29b65f0bb3b5ffe0623bc69df
Najma Elmi <nelmi199@gmail.com>
2021-12-06 16:46:46 +0000

    task 2d complete

--- index.js
+++ index.js
@@ -16,4 +16,24 @@ let myCollection = [
   },
 ];
 
-console.log(myCollection);
+/* console.log(myCollection); */
+
+function describeItem(item) {
+if (item.count === 1) {
+  console.log(`I have a ${item.name}. Here's what I like about it: ${item.whatILike}`);
+} else {
+  console.log(`I have ${item.count, item.name}s. Here's what I like about them: ${item.whatILike}`);
+}
+}
+
+//describeItem(myCollection[0]);
+
+function describeCollection(arr) {
+  for(let i = 0; i < arr.length; i++) {
+    let wholeCollection = arr[i];
+    describeItem(wholeCollection);
+    console.log("This is working");
+  }
+}
+
+describeCollection(myCollection);
